95f2ccd46e92258e4b37e54b826ec7ab4e701aa0
DG Hades <61195031+DGHades@users.noreply.github.com>
2022-03-15 20:54:38 +0100

    Added Particle on Destruction

--- Assets/Bullet.cs
+++ Assets/Bullet.cs
@@ -5,6 +5,8 @@ using UnityEngine;
 public class Bullet : MonoBehaviour
 {
     // Start is called before the first frame update
+    GameObject bullet;
+    public GameObject particleObject;
     void Start()
     {
         
@@ -22,7 +24,9 @@ public class Bullet : MonoBehaviour
     void OnTriggerEnter2D(Collider2D coll)
     {
         if (coll.gameObject.tag == "Target") {
+            SpawnAnim(coll.gameObject);
             Destroy(coll.gameObject);
+
             Destroy(gameObject);
             
         }
@@ -31,6 +35,32 @@ public class Bullet : MonoBehaviour
             
             Destroy(gameObject);
         }
+
+    }
+
+    void SpawnAnim(GameObject g)
+    {
+        int i = 0;
+        do
+        {
+            float rangeX = Random.Range(-5, 5);
+            float rangeY = Random.Range(-5, 5);
+
+            Vector3 newPosition = new Vector3(g.transform.position.x + rangeX, g.transform.position.y + rangeY);
+            GameObject t = (GameObject)(Instantiate(particleObject, newPosition, Quaternion.identity));
+
+            t.GetComponent<Rigidbody2D>().AddForce((t.transform.position- g.transform.position) * 50);
+
+            Vector3 dir = g.transform.position - t.transform.position;
+
+            float angle = Mathf.Atan2(dir.y, dir.x) * Mathf.Rad2Deg - 90;
+
+            t.transform.rotation = Quaternion.AngleAxis(angle, Vector3.forward);
+
+
+            i++;
+        } while (i < 30);
+
 
     }
 }
---
+++ Assets/ParticleDestruction.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ParticleDestruction : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        Destroy(gameObject, 0.5f);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Vector2 v = GetComponent<Rigidbody2D>().velocity;
+        v = v.normalized;
+        v *= 15;
+        GetComponent<Rigidbody2D>().velocity = v;
+    }
+
+
+}
