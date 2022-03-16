605aaa6daca20524ce6e6642b87ae586512f87d6
DG Hades <61195031+DGHades@users.noreply.github.com>
2022-03-16 09:03:09 +0100

    Updated Destruction Particle

--- Assets/ParticleDestruction.cs
+++ Assets/ParticleDestruction.cs
@@ -13,10 +13,12 @@ public class ParticleDestruction : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+        /*
         Vector2 v = GetComponent<Rigidbody2D>().velocity;
         v = v.normalized;
         v *= 15;
         GetComponent<Rigidbody2D>().velocity = v;
+        */
     }
 
 
