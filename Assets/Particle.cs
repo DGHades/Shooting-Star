54fdbf995cdf2deb1b2b29000b87c19631d8d3db
DG Hades <61195031+DGHades@users.noreply.github.com>
2022-03-17 17:24:17 +0100

    Cleanup Particle Script Added Comments

--- Assets/Particle.cs
+++ Assets/Particle.cs
@@ -5,23 +5,20 @@ using UnityEngine;
 public class Particle : MonoBehaviour
 {
     // Start is called before the first frame update
-    
     void Start()
     {
         
     }
-
     // Update is called once per frame
-
     private void OnTriggerEnter2D(Collider2D collision)
     {
+        //Delete if hits Unspawned Object
         if (collision.gameObject.tag == "Unspawned" || collision.gameObject.tag == "UnspawnedStar")
         {
             Destroy(gameObject);
         }
-        
     }
-    void Update()
+    void FixedUpdate()
     {
       
     }
