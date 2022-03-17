71cc968b1bccf554860acd03c1f0f0ff20035ab9
DG Hades <61195031+DGHades@users.noreply.github.com>
2022-03-17 11:36:25 +0100

    Cleanup Global Variable script Added Comments

--- Assets/GlobalVariable.cs
+++ Assets/GlobalVariable.cs
@@ -5,13 +5,18 @@ using UnityEngine;
 public class GlobalVariable : MonoBehaviour
 {
     // Start is called before the first frame update
+    //Declare every needed Global Variable
     public static float attackspeed;
     public static float attackDmg;
     public static bool spawnStars;
+
+    //For future uses
     GameObject PlayerChecker;
     Vector2 PlayerPos;
     void Start()
     {
+        //Set every Gloabl Variable back to standard because
+        //Global Variable script is started on Respawn
         attackspeed = 1;
         attackDmg = 100;
         spawnStars = true;
