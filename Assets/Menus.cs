29283f8ca3231c7eecd96151679f232643fa99c2
DG Hades <61195031+DGHades@users.noreply.github.com>
2022-03-17 11:50:53 +0100

    Cleanup Menus script Added Comments

--- Assets/Menus.cs
+++ Assets/Menus.cs
@@ -4,44 +4,41 @@ using UnityEngine;
 
 public class Menus : MonoBehaviour
 {
-    // Start is called before the first frame update
-     public float timeStamp;
+    
+    public float timeStamp;
     GameObject[] RespawnObjects;
-   
+    // Start is called before the first frame update
     void Start()
     {
+        //Start timer with 2 Sec offset due to "FindGameObjectsWithTag" in other scripst not working
+        //when gameobject disabled too early
         timeStamp = Time.time + 2;
         RespawnObjects = GameObject.FindGameObjectsWithTag("RespawnMenu");
-        
     }
 
     // Update is called once per frame
     void FixedUpdate()
     {
-
+        //Hide menu after time Offset
         if (timeStamp >= Time.time)
         {
             HideRespawnMenu();
         }
     }
-
+    //Explains itself
     void HideRespawnMenu() 
     {
         foreach (GameObject g in RespawnObjects)
         {
-            
             g.SetActive(false);
-
         }
     }
-
+    //Explains itself
     public void ShowRespawnMenu()
     {
         foreach (GameObject g in RespawnObjects)
         {
-            
             g.SetActive(true);
-
         }
     }
 }
