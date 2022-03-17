8a22863c3469f240f724cf4d9e6a87340e98d196
DG Hades <61195031+DGHades@users.noreply.github.com>
2022-03-17 17:23:25 +0100

    Cleanup MovingBackground Added Comments

--- Assets/MovingBackground.cs
+++ Assets/MovingBackground.cs
@@ -8,20 +8,19 @@ public class MovingBackground : MonoBehaviour
     Vector3 offset = new Vector3();
     void Start()
     {
+        //Set starting Position for Background Objects
+        //DO NOT CHANGE IF NOT NEEDED
         offset = new Vector3(0, 10, 0);
     }
-
     // Update is called once per frame
     void FixedUpdate()
     {
+        //Move Background Object down
         gameObject.transform.Translate(Vector3.down * 0.05f);
-
+        //if hits specified position set back to starting Position
         if (gameObject.transform.position.y <= -20) 
         {
             gameObject.transform.position = offset;
         }
-
-
-
     }
 }
