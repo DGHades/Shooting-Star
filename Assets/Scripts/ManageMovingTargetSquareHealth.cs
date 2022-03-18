22fde61fdb4035d03af50a53b912623a223debc2
DG Hades <61195031+DGHades@users.noreply.github.com>
2022-03-18 18:37:21 +0100

    Added better Particle system on Destruction

--- Assets/Scripts/ManageMovingTargetSquareHealth.cs
+++ Assets/Scripts/ManageMovingTargetSquareHealth.cs
@@ -7,6 +7,10 @@ public class ManageMovingTargetSquareHealth : MonoBehaviour
     public float health, type;
     public static int TARGET_BOULDER;
     public GameObject particleObject;
+    public ParticleSystem particleSystem;
+    public SpriteRenderer sr;
+    float dur;
+    
     // Start is called before the first frame update
     void Start()
     {
@@ -17,6 +21,10 @@ public class ManageMovingTargetSquareHealth : MonoBehaviour
         {
             health = 100;
         }
+
+        
+        
+        
     }
 
     // Update is called once per frame
@@ -32,10 +40,9 @@ public class ManageMovingTargetSquareHealth : MonoBehaviour
         if (health <= 0)
         {
             //Do destroy Animation before Destroying Object
-            DestroyAnim(gameObject);
             GlobalVariable.score++;
             GlobalVariable.fillbarValue++;
-            destroyTarget();
+            DestroyAnim(gameObject);
         }
 
     }
@@ -47,31 +54,16 @@ public class ManageMovingTargetSquareHealth : MonoBehaviour
     }
     void DestroyAnim(GameObject g)
     {
-        //Counter
-        int i = 0;
-        do
-        {
-            //Get Random X and Y Vector to get random directions for Particles
-            float rangeX = Random.Range(-1f, 1f);
-            float rangeY = Random.Range(-1f, 1f);
-            //Create Vector for Direction of Particle
-            Vector3 newPosition = new Vector3(g.transform.position.x + rangeX, g.transform.position.y + rangeY);
-            //Create object at Declared position 
-            GameObject t = (GameObject)(Instantiate(particleObject, newPosition, Quaternion.identity));
-            //Add force away from Target Object
-            t.GetComponent<Rigidbody2D>().AddForce((t.transform.position - g.transform.position) * 50);
-            //Calculation for Rotation so it Faces away from Target Object
-            Vector3 dir = g.transform.position - t.transform.position;
-            float angle = Mathf.Atan2(dir.y, dir.x) * Mathf.Rad2Deg - 90;
-            //Do Rotation
-            t.transform.rotation = Quaternion.AngleAxis(angle, Vector3.forward);
+        dur = particleSystem.main.duration;
+        var em = particleSystem.emission;
 
-            //Count
-            i++;
-            //Do 30 Particles
-            //was a good Number Change if looks better
-        } while (i < 30);
+        em.enabled = true;
+        particleSystem.Play();
 
+        Destroy(sr);
+        Invoke(nameof(destroyTarget), dur);
+
+       
 
     }
 }
--- Assets/Scripts/ManageMovingTargetStarHealth.cs
+++ Assets/Scripts/ManageMovingTargetStarHealth.cs
@@ -7,6 +7,9 @@ public class ManageMovingTargetStarHealth : MonoBehaviour
     public float health, type;
     public static int TARGET_BOULDER;
     public GameObject particleObject;
+    public ParticleSystem particleSystem;
+    public SpriteRenderer sr;
+    float dur;
     // Start is called before the first frame update
     void Start()
     {
@@ -47,30 +50,14 @@ public class ManageMovingTargetStarHealth : MonoBehaviour
     }
     void DestroyAnim(GameObject g)
     {
-        //Counter
-        int i = 0;
-        do
-        {
-            //Get Random X and Y Vector to get random directions for Particles
-            float rangeX = Random.Range(-1f, 1f);
-            float rangeY = Random.Range(-1f, 1f);
-            //Create Vector for Direction of Particle
-            Vector3 newPosition = new Vector3(g.transform.position.x + rangeX, g.transform.position.y + rangeY);
-            //Create object at Declared position 
-            GameObject t = (GameObject)(Instantiate(particleObject, newPosition, Quaternion.identity));
-            //Add force away from Target Object
-            t.GetComponent<Rigidbody2D>().AddForce((t.transform.position - g.transform.position) * 50);
-            //Calculation for Rotation so it Faces away from Target Object
-            Vector3 dir = g.transform.position - t.transform.position;
-            float angle = Mathf.Atan2(dir.y, dir.x) * Mathf.Rad2Deg - 90;
-            //Do Rotation
-            t.transform.rotation = Quaternion.AngleAxis(angle, Vector3.forward);
+        dur = particleSystem.main.duration;
+        var em = particleSystem.emission;
 
-            //Count
-            i++;
-            //Do 30 Particles
-            //was a good Number Change if looks better
-        } while (i < 30);
+        em.enabled = true;
+        particleSystem.Play();
+
+        Destroy(sr);
+        Invoke(nameof(destroyTarget), dur);
 
 
     }
