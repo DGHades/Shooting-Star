58e090ab41704e78329b908475bbd00a00c8e244
M0rG <54027806+Cedylol2002@users.noreply.github.com>
2022-03-19 14:35:57 +0100

    Fixed Menue due to (ScriptLoading/Deleting)

--- Assets/Scripts/Player/Player.cs
+++ Assets/Scripts/Player/Player.cs
@@ -5,7 +5,7 @@ public class Player : MonoBehaviour
 {
     private bool stopTop = false, stopBot = false, stopLeft = false, stopRight = false;
     public GameObject playerOptic;
-    public Canvas DieMenue;
+    public Canvas BeforeDieMenue;
     public Canvas BeforeStartMenue;
     public float attackDmg, attackspeed;
     Vector3 _origPos = new Vector3();
@@ -13,7 +13,7 @@ public class Player : MonoBehaviour
     private void Start()
     {
         BeforeStartMenue.gameObject.SetActive(true);
-        DieMenue.gameObject.SetActive(false);
+        BeforeDieMenue.gameObject.SetActive(false);
     }
     private void OnTriggerExit2D(Collider2D other)
     {
@@ -57,7 +57,7 @@ public class Player : MonoBehaviour
             stopBot = true;
             stopLeft = true;
             stopRight = true;
-            DieMenue.gameObject.SetActive(true);
+            BeforeDieMenue.gameObject.SetActive(true);
         }
         if (collision.gameObject.name == "BorderTop")
         {
