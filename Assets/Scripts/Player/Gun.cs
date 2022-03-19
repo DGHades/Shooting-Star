10771862a8c27f5c3c73350dcec8e6b93a97a2fa
11tuvork28 <zofi@xamh.de>
2022-03-19 09:35:36 +0100

    moved player related scripts into own folder

---
+++ Assets/Scripts/Player/Gun.cs
@@ -0,0 +1,40 @@
+﻿using UnityEngine;
+
+public class Gun : MonoBehaviour
+{
+    public float timeStamp;
+    public GameObject bulletObj;
+    // Start is called before the first frame update
+    public Player player;
+
+    void OnTriggerStay2D(Collider2D target)
+    {
+        //On Collision with Target, Player Object gest Destroyed aka dies
+        //and Activate Respawn Button/Menu before
+        // TODO
+        if (target.gameObject.tag == "TargetSquare" || target.gameObject.tag == "TargetStar")
+        {
+            Debug.Log("Shot: " + target.gameObject.tag);
+            shoot(target);
+        }
+    }
+    public void shoot(Collider2D coll)
+    {
+        //only shoot when cooldown is over
+        if (timeStamp <= Time.time)
+        {
+            //Set new time when shooting is available again
+            timeStamp = Time.time + player.attackspeed;
+            GameObject bullet = (GameObject)(Instantiate(bulletObj, player.transform.position, Quaternion.identity));
+            ((BaseBullet)bullet.GetComponent("BaseBullet")).attackDmg = player.attackDmg;
+            bullet.GetComponent<Rigidbody2D>().AddForce((coll.transform.position - player.transform.position) * 200);
+            //Calculate Rotation to Target direction
+            Vector3 dir = coll.transform.position - bullet.transform.position;
+            float angle = Mathf.Atan2(dir.y, dir.x) * Mathf.Rad2Deg;
+            //Set rotation
+            bullet.transform.rotation = Quaternion.AngleAxis(angle, Vector3.forward);
+        }
+    }
+
+
+}
---
+++ Assets/Scripts/Player/PlayerOpticTurn.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerOpticTurn : MonoBehaviour
+{
+    Vector3 _origPos = new Vector3();
+    // Start is called before the first frame update
+    void Start()
+    {
+       
+    }
+    private void OnTriggerExit2D(Collider2D other)
+    {
+        //Change player looking direction in Bullet shooting direction
+        if (other.gameObject.tag == "Bullet")
+        {
+            //Get Direction
+            Vector3 dir = other.transform.position - transform.position;
+            //calculate rotation
+            float angle = Mathf.Atan2(dir.y, dir.x) * Mathf.Rad2Deg - 90;
+            //set rotation
+            transform.rotation = Quaternion.AngleAxis(angle, Vector3.forward);
+        }
+    }
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+        //Change player looking direction in moving direction
+        Vector3 moveDirection = gameObject.transform.position - _origPos;
+        //only do if player is moving
+        if (moveDirection != Vector3.zero)
+        {
+            //calculation for rotation
+            float angle = Mathf.Atan2(moveDirection.y, moveDirection.x) * Mathf.Rad2Deg - 90;
+            transform.rotation = Quaternion.AngleAxis(angle, Vector3.forward);
+        }
+        //position behind player looking direction because calculation actually 
+        //calculates from != player moving direction
+        _origPos = gameObject.transform.position;
+    }
+}
