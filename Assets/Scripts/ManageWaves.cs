071c9ecbe61e057d6dddbd0d78b8a9fcba95b5ac
M0rG <54027806+Cedylol2002@users.noreply.github.com>
2022-03-22 23:18:06 +0100

    Added Shake and Glitch Effect

--- Assets/Scripts/ManageWaves.cs
+++ Assets/Scripts/ManageWaves.cs
@@ -1,7 +1,9 @@
+using System.Collections;
+using System.Collections.Generic;
 using UnityEngine;
-
 public class ManageWaves : MonoBehaviour
 {
+    public AnalogGlitch Glitch;
     // Start is called before the first frame update
     void Start()
     {
@@ -13,11 +15,26 @@ public class ManageWaves : MonoBehaviour
     {
         if (GlobalVariable.fillbarValue >= GlobalVariable.waveScore)
         {
-
             GlobalVariable.fillbarMin = 0;
             GlobalVariable.waveScore *= 1.5f;
             GlobalVariable.fillbarValue = 0;
             GlobalVariable.waveCount++;
+
+            float duration = 0.1f;
+            StartCoroutine(Shake(duration));
         }
+    }
+    public IEnumerator Shake(float duration)
+    {
+        float elapsedTime = 0;
+        while (elapsedTime < duration)
+        {
+            Glitch.colorDrift = 0.854f;
+            Glitch.scanLineJitter = 1f;
+            elapsedTime += Time.deltaTime;
+            yield return null;
+        }
+        Glitch.colorDrift = 0f;
+        Glitch.scanLineJitter = 0f;
     }
 }
