b5feef6faea3c8515a97b430e2e1f936e64be504
11tuvork28 <zofi@xamh.de>
2022-03-25 22:40:59 +0100

    Add back Player hit box fix and rework

--- Assets/Scripts/Player/Player.cs
+++ Assets/Scripts/Player/Player.cs
@@ -2,25 +2,29 @@
 using UnityEngine.SceneManagement;
 public class Player : MonoBehaviour
 {
-    private bool stopTop = false, stopBot = false, stopLeft = false, stopRight = false;
-    public GameObject playerOptic;
+    public bool stopTop = false, stopBot = false, stopLeft = false, stopRight = false;
     public Canvas BeforeDieMenue;
     public Canvas BeforeStartMenue;
     public ParticleSystem spawnBurstParticleSystem;
     public AnalogGlitch analog;
     public ParticleSystem spawnExplosionParticleSystem;
     public GameObject EnemyPos;
-    public PolygonCollider2D Coll2D;
+    public PolygonCollider2D test;
     public float attackDmg, attackspeed;
     Vector3 _origPos = new Vector3();
     bool once = false;
     bool onceTwo = false;
 
+    [SerializeField]
+    private Gun gun;
+    [SerializeField]
+    private PlayerOpticTurn playerOptic;
+
     private void Start()
     {
         BeforeStartMenue.gameObject.SetActive(true);
         BeforeDieMenue.gameObject.SetActive(false);
-        Coll2D = gameObject.GetComponent<PolygonCollider2D>();
+        test = gameObject.GetComponent<PolygonCollider2D>();
     }
 
     private void StartAnim()
@@ -31,17 +35,17 @@ public class Player : MonoBehaviour
 
         em.enabled = true;
         spawnBurstParticleSystem.Play();
-       
-       
-         em = spawnExplosionParticleSystem.emission;
+
+
+        em = spawnExplosionParticleSystem.emission;
 
         em.enabled = true;
         spawnExplosionParticleSystem.Play();
 
         Invoke(nameof(StopAnim), 0.2f);
-        
+
     }
-    private void StopAnim() 
+    private void StopAnim()
     {
         var em = spawnBurstParticleSystem.emission;
         em.enabled = false;
@@ -50,83 +54,12 @@ public class Player : MonoBehaviour
         analog.colorDrift = 0;
         analog.scanLineJitter = 0;
     }
-    private void OnTriggerExit2D(Collider2D other)
-    {
-        //Re-Activate movement in direction that has been blocked
-        if (other.gameObject.name == "BorderTop")
-        {
-            stopTop = false;
-        }
-        if (other.gameObject.name == "BorderBottom")
-        {
-            stopBot = false;
-        }
-        if (other.gameObject.name == "BorderLeft")
-        {
-            stopLeft = false;
-        }
-        if (other.gameObject.name == "BorderRight")
-        {
-            stopRight = false;
-        }
-        if (other.gameObject.tag == "Bullet")
-        {
-            //Get Direction
-            Vector3 dir = other.transform.position - playerOptic.transform.position;
-            //calculate rotation
-            float angle = Mathf.Atan2(dir.y, dir.x) * Mathf.Rad2Deg - 90;
-            //set rotation
-            playerOptic.transform.rotation = Quaternion.AngleAxis(angle, Vector3.forward);
-        }
-        if (other.gameObject.tag == "Trigger")
-        {
-                analog.scanLineJitter = 0;
-        }
-    }
-
-    void OnTriggerEnter2D(Collider2D collision)
-    {
-        if (collision.gameObject.tag.StartsWith("Trigger"))
-        {
-                analog.scanLineJitter = 0.3f;
-        }
-        //On Collision with Target, Player Object gest Destroyed aka dies
-        //and Activate Respawn Button/Menu before
-        if (collision.gameObject.tag.StartsWith("Target"))
-        {
-            attackDmg = 0;
-            attackspeed = 999999999;
-            stopTop = true;
-            stopBot = true;
-            stopLeft = true;
-            stopRight = true;
-            BeforeDieMenue.gameObject.SetActive(true);
-            GlobalVariable.stopGame = true;
-            gameObject.SetActive(false);
-        }
-        if (collision.gameObject.name == "BorderTop")
-        {
-            stopTop = true;
-        }
-        if (collision.gameObject.name == "BorderBottom")
-        {
-            stopBot = true;
-        }
-        if (collision.gameObject.name == "BorderLeft")
-        {
-            stopLeft = true;
-        }
-        if (collision.gameObject.name == "BorderRight")
-        {
-            stopRight = true;
-        }
-    }
 
     // Update is called once per frame
     // Controls
     void FixedUpdate()
     {
-        Coll2D.transform.rotation = gameObject.transform.rotation;
+        test.transform.rotation = gameObject.transform.rotation;
         if (GlobalVariable.startGame && !once)
         {
             StartAnim();
@@ -141,28 +74,33 @@ public class Player : MonoBehaviour
         {
             onceTwo = false;
         }
+        // Bit shift the index of the Enemy layer (8) to get a bit mask
+        int layerMask = 1 << 8;
+        Collider2D coll = Physics2D.OverlapCircle(gameObject.transform.position, 5f, layerMask);
+        if (coll != null)
+            gun.shoot(coll.gameObject);
+
         //Movement
         //Block input on Border hit
         if (GlobalVariable.startGame)
         {
-        if (Input.GetKey(KeyCode.LeftArrow) && stopLeft == false)
-        {
-            gameObject.transform.Translate(Vector3.left * 0.1f);
+            if (Input.GetKey(KeyCode.LeftArrow) && stopLeft == false)
+            {
+                gameObject.transform.Translate(Vector3.left * 0.1f);
+            }
+            if (Input.GetKey(KeyCode.UpArrow) && stopTop == false)
+            {
+                gameObject.transform.Translate(Vector3.up * 0.1f);
+            }
+            if (Input.GetKey(KeyCode.DownArrow) && stopBot == false)
+            {
+                gameObject.transform.Translate(Vector3.down * 0.1f);
+            }
+            if (Input.GetKey(KeyCode.RightArrow) && stopRight == false)
+            {
+                gameObject.transform.Translate(Vector3.right * 0.1f);
+            }
         }
-        if (Input.GetKey(KeyCode.UpArrow) && stopTop == false)
-        {
-            gameObject.transform.Translate(Vector3.up * 0.1f);
-        }
-        if (Input.GetKey(KeyCode.DownArrow) && stopBot == false)
-        {
-            gameObject.transform.Translate(Vector3.down * 0.1f);
-        }
-        if (Input.GetKey(KeyCode.RightArrow) && stopRight == false)
-        {
-            gameObject.transform.Translate(Vector3.right * 0.1f);
-        }
-        }
-        
         //Change player looking direction in moving direction
         Vector3 moveDirection = playerOptic.gameObject.transform.position - _origPos;
         //only do if player is moving
@@ -175,6 +113,18 @@ public class Player : MonoBehaviour
         //position behind player looking direction because calculation actually 
         //calculates from != player moving direction
         _origPos = playerOptic.gameObject.transform.position;
+    }
 
+    public void Die()
+    {
+        attackDmg = 0;
+        attackspeed = 999999999;
+        stopTop = true;
+        stopBot = true;
+        stopLeft = true;
+        stopRight = true;
+        BeforeDieMenue.gameObject.SetActive(true);
+        GlobalVariable.stopGame = true;
+        gameObject.SetActive(false);
     }
 }
